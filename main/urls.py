7bb2f41a6ecb07cc6f6effb6b9a3aadc00798614
Joe Philip <joe.mj007@gmail.com>
2025-08-09 16:49:39 +0000

    Added login API

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from rest_framework.authtoken import views as rest_framework_views
 
 from . import views
 
 urlpatterns = [
-    path('signup', views.SignupAPIView.as_view())
+    path('signup', views.SignupAPIView.as_view()),
+    path(
+        'login', rest_framework_views.obtain_auth_token,
+        name='login'
+    ),
 ]
