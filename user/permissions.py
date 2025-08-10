cba2a499c7edf3695a28b36eb1e67a681a278721
Joe Philip <joe.mj007@gmail.com>
2025-08-10 08:06:44 +0000

    Defined IsContanctGroupOwner

---
+++ user/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import BasePermission
+from rest_framework.request import Request
+from rest_framework.views import View
+
+
+class IsContanctGroupOwner(BasePermission):
+    """
+    Custom permission to only allow owners of a contact group to access it.
+    """
+
+    def has_object_permission(self, request: Request, view: View, obj) -> bool:
+        # Check if the user is the owner of the contact group
+        return obj.owner == request.user
