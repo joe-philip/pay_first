f8fe52f313c1531577f8b1386d627be81b6f831f
Joe Philip <joe.mj007@gmail.com>
2025-08-17 09:58:19 +0000

    Modified exception_handler
    added 404 status for Http404 exceptions

--- root/utils/exception_handlers.py
+++ root/utils/exception_handlers.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 
@@ -17,4 +18,6 @@ def exception_handler(exc: Exception, context: dict):
     """
     if isinstance(exc, APIException):
         return Response(fail(exc.detail), status=exc.status_code)
+    elif isinstance(exc, Http404):
+        return Response(fail(str(exc)), status=404)
     return Response(fail(str(exc)), status=500)
