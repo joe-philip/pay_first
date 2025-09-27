4332d91236734f2f4a570dc91bf39fb0d6248b55
Joe Philip <joe.mj007@gmail.com>
2025-09-27 16:59:41 +0000

    Defined AppSettings model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class AppSettings(models.Model):
+    app_name = models.CharField(max_length=50)
+    version = models.CharField(max_length=10)
+    tag = models.CharField(max_length=50, help_text="Commit hash on tag")
+    change_log = models.TextField()
+    terms_and_conditions = models.TextField()
+
+    class Meta:
+        db_table = "app_settings"
+        verbose_name = "App setting"
