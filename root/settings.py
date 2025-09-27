d333af1b646def288f5b442296cabb4d58ca56e0
Joe Philip <joe.mj007@gmail.com>
2025-09-27 16:02:03 +0530

    Added config for static files (#77)

--- root/settings.py
+++ root/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from pymysql import install_as_MySQLdb
 import os
 from datetime import timedelta
 from pathlib import Path
@@ -21,7 +22,6 @@ env = Env(
     DEBUG=(bool, False)
 )
 
-from pymysql import install_as_MySQLdb
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,6 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
