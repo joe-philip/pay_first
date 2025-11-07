b18b3d6b549ef3a54f19cfa0ed25473cc7dd0afd
Joe Philip <joe.mj007@gmail.com>
2025-11-07 17:43:25 +0000

    Defined ModuleInfo model

--- main/models.py
+++ main/models.py
@@ -16,3 +16,21 @@ class AppSettings(MetaModel):
     class Meta:
         db_table = "app_settings"
         verbose_name = "App setting"
+
+
+class ModuleInfo(MetaModel):
+    name = models.CharField(max_length=100)
+    name_plural = models.CharField(max_length=110, null=True, blank=True)
+    model = models.OneToOneField(
+        "contenttypes.ContentType",
+        related_name="module_info",
+        on_delete=models.CASCADE
+    )
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "module_info"
+        verbose_name = "Module info"
+
+    def __str__(self) -> str: return self.name
