5cf67ea2cde034fc0178e01d71464466a0d50a1d
Joe Philip <joe.mj007@gmail.com>
2025-11-07 17:43:54 +0000

    Registered ModuleInfo model into admin app

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import AppSettings
+from .models import AppSettings, ModuleInfo
 
 # Register your models here.
 
 admin.site.register(AppSettings)
+admin.site.register(ModuleInfo)
