4f51c1efa5942b60a9c9dd040812ad61b39f323b
Joe Philip <joe.mj007@gmail.com>
2026-02-10 18:44:37 +0000

    Defined task send_verification_email_task

---
+++ main/tasks.py
@@ -0,0 +1,45 @@
+from datetime import timedelta
+
+from celery import shared_task
+from django.conf import settings
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
+from main.models import AppSettings
+
+from .models import User
+
+
+@shared_task(
+    bind=True,
+    autoretry_for=(Exception,),
+    retry_kwargs={"max_retries": 5, "countdown": 30},
+    retry_backoff=True,
+    retry_jitter=True,
+)
+def send_verification_email_task(self, user_id: int):
+    user = User.objects.get(id=user_id)
+    token = default_token_generator.make_token(user)
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    link = settings.EMAIL_VERIFICATION_URL.format(uid=uid, token=token)
+    timeout = timedelta(seconds=settings.PASSWORD_RESET_TIMEOUT)
+    app_settings = AppSettings.objects.last()
+    app_title = app_settings.app_name if app_settings else "PayBuddy"
+    send_mail(
+        subject=f"{app_title}: Email Verification",
+        message="",
+        html_message=render_to_string(
+            "email/welcome.html",
+            {
+                "user": user,
+                "link": link,
+                "expiry": int(timeout.total_seconds() / 3600),
+                "app_title": app_title,
+            },
+        ),
+        from_email=None,
+        recipient_list=[user.username],
+    )
